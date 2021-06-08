0d10d88ddbf0a8173276f306eca28f59866ac540
miltonzini <miltonzini@gmail.com>
2021-06-08 12:47:14 -0300

    overlay
    -overlay for open hamburger
    -animations

--- temp/test/landing/js/script.js
+++ temp/test/landing/js/script.js
@@ -1,12 +1,18 @@
 const btnHamburguer = document.querySelector('#btnHamburguer');
+const header = document.querySelector('.header');
+const overlay = document.querySelector('.overlay')
 
 btnHamburguer.addEventListener('click', function(){
     console.log('open haburguer');
 
-    if (btnHamburguer.classList.contains('open')){
-        btnHamburguer.classList.remove('open');
+    if (header.classList.contains('open')){ // close Hamburger menu 
+        header.classList.remove('open');
+        overlay.classList.remove('fade-in');
+        overlay.classList.add('fade-out');
     }
-    else {
-        btnHamburguer.classList.add('open');
+    else { // Open Hamburger menu  
+        header.classList.add('open');
+        overlay.classList.remove('fade-out');
+        overlay.classList.add('fade-in');
     }
 });
