33c05a62d4291035292ed07322c4139a8d3346e6
miltonzini <miltonzini@gmail.com>
2021-06-11 20:47:03 -0300

    close overlay on click

--- temp/test/landing/js/script.js
+++ temp/test/landing/js/script.js
@@ -1,8 +1,9 @@
-const btnHamburguer = document.querySelector('#btnHamburguer');
-const body = document.querySelector('body');
+const btnHamburguer = document.querySelector('#btnHamburguer');const body = document.querySelector('body');
 const header = document.querySelector('.header');
 const overlay = document.querySelector('.overlay');
 const fadeElems = document.querySelectorAll('.has-fade');
+const headerAnchor = document.querySelectorAll('.header__menu__anchor');
+
 
 btnHamburguer.addEventListener('click', function(){
     console.log('open haburguer');
@@ -22,7 +23,31 @@ btnHamburguer.addEventListener('click', function(){
         fadeElems.forEach(function(element){
             element.classList.remove('fade-out');
             element.classList.add('fade-in');
-        });
-        
+        });    
     }
+});
+
+// Cerrar Overlay al clickear anchor
+headerAnchor.forEach((anchor) => {
+    anchor.addEventListener('click', () => {
+        console.log("anchor clicked");
+
+        if (header.classList.contains('open')){ // close Hamburger menu 
+            body.classList.remove('noscroll');
+            header.classList.remove('open');
+            fadeElems.forEach(function(element){
+                element.classList.remove('fade-in');
+                element.classList.add('fade-out');    
+            });
+            
+        }
+        else { // Open Hamburger menu  
+            body.classList.add('noscroll');
+            header.classList.add('open');
+            fadeElems.forEach(function(element){
+                element.classList.remove('fade-out');
+                element.classList.add('fade-in');
+            });    
+        }
+    });
 });
